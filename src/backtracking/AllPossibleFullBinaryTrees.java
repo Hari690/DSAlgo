a7c3554cdb44556064aed598aac454d5eca67b5b
Harigovind Raveendran <hari_ooehs@yahoo.com>
2023-07-23 11:07:13 +0100

    Update AllPossibleFullBinaryTrees.java with cache

--- src/backtracking/AllPossibleFullBinaryTrees.java
+++ src/backtracking/AllPossibleFullBinaryTrees.java
@@ -11,39 +11,41 @@ import trees.TreeNode;
  * A full binary tree is a binary tree where each node has exactly 0 or 2 children.
  */
 public class AllPossibleFullBinaryTrees {
+    Map<Integer,List<TreeNode>> cache = new HashMap<>();
     public List<TreeNode> allPossibleFBT(int N) {
         // Recursive: build all possible FBT of leftSubTree and rightSubTree with number n
-        if(N <= 0 || N % 2 == 0) return new ArrayList<>();
-
+        if(N <= 0) return new ArrayList<>();
+        
+        if(cache.containsKey(N))
+            return cache.get(N);
+        
         //1. if N = 3 , the number of nodes combination are as follows
         //      left    root    right
-        //       1       1        1
+        //       1       1        1 
         //--------------N = 3, res = 1----------
-
+        
         //2. if N = 5 , the number of nodes combination are as follows
         //      left    root    right
         //       1       1        3 (recursion)
-        //       3       1        1
+        //       3       1        1 
         //  --------------N = 5, res = 1 + 1 = 2----------
-
+        
         //3. if N = 7 , the number of nodes combination are as follows
         //      left    root    right
         //       1       1        5 (recursion)
-        //       3       1        3
+        //       3       1        3 
         //       5       1        1
         //  --------------N = 7, res = 2 + 1 + 2 = 5----------
-
+        
         //4. in order to make full binary tree, the node number must increase by 2
         List<TreeNode> res = new ArrayList<>();
-        // base condition.
         if(N == 1) {
             res.add(new TreeNode(0));
             return res;
         }
-        for(int i = 1; i < N; i += 2) {
+        for(int i = 1; i < N; i ++) {
             List<TreeNode> leftSubTrees = allPossibleFBT(i);
             List<TreeNode> rightSubTrees = allPossibleFBT(N - i - 1);
-            // generate all combinations.
             for(TreeNode l : leftSubTrees) {
                 for(TreeNode r : rightSubTrees) {
                     TreeNode root = new TreeNode(0);
@@ -53,6 +55,7 @@ public class AllPossibleFullBinaryTrees {
                 }
             }
         }
+        cache.put(N, res);
         return res;
     }
 
